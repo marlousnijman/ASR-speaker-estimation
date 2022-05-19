Fixed steps according to paper and add normalization

src/dataset.py
@@ -4,32 +4,43 @@ import random
 import numpy as np
 import pyloudnorm as pyln
 import soundfile as sf
+import util
+from tqdm import tqdm
 
 
-def make_dataset(input_dir, output_dir, max_k):
+def make_dataset(input_dir, output_dir, max_k, samples=1820):
     """
-    Make a dataset by combining audio files for different
-    numbers of speakers.
+    Create a dataset with an equal number of samples for each
+    k = 0, ..., max_k, by adding k speech samples from randomly
+    selected unique speakers, and peak normalize the resulting
+    audio.
     """
-    all_speakers = os.listdir(input_dir)
+    speakers = util.get_speakers(input_dir, file_format=".wav")
+    files = os.listdir(input_dir)
 
-    # Compute how many samples per k we can make
-    # with the available dataset
-    n = np.sum(np.arange(max_k+1))
-    samples = len(all_speakers) // n
-
-    for k in range(1, max_k+1):
+    for k in tqdm(range(1, max_k+1)):
         for s in range(samples):
-            speakers = random.sample(all_speakers, k)
+            # Select k random speakers
+            k_speakers = random.sample(speakers, k)
             all_audio = []
 
-            # Collect k random speakers
-            for speaker in speakers:
-                audio, sample_rate = sf.read(os.path.join(input_dir, speaker))
-                all_audio.append(audio)
+            # Select speaker audio
+            for speaker in k_speakers:
+                speaker_files = [f for f in files if f.startswith(speaker)]
 
-                # Remove used samples
-                all_speakers.remove(speaker)
+                # Remove speakers with no remaining audio samples
+                if len(speaker_files) == 1:
+                    speakers.remove(speaker)
+
+                # Select random audio file
+                speaker_file = random.choice(speaker_files)
+
+                # Remove audio file from list
+                files.remove(speaker_file)
+
+                # Read audio
+                audio, sample_rate = sf.read(os.path.join(input_dir, speaker_file))
+                all_audio.append(audio)
 
             # Combine audio
             all_audio = np.sum(all_audio, axis=0)
@@ -39,8 +50,6 @@ def make_dataset(input_dir, output_dir, max_k):
 
             # Write to file
             sf.write(os.path.join(output_dir, f"{k:02}_speakers_{s}.wav"), normalized_audio, sample_rate)
-
-    return samples
 
 
 def main(args):
@@ -54,7 +63,7 @@ def main(args):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    make_dataset(input_dir, output_dir, args.max_k)
+    make_dataset(input_dir, output_dir, args.max_k, args.samples)
 
 
 if __name__ == "__main__":
@@ -67,6 +76,7 @@ if __name__ == "__main__":
 
     # Parameters
     parser.add_argument('-k', '--max_k', type=int, help="Maximum number of speakers", default = 10)
+    parser.add_argument('-s', '--samples', type=int, help="Number of samples for each speaker", default = 1820)
 
     args = parser.parse_args()
 

src/util.py
@@ -4,6 +4,9 @@ import random
 import numpy as np
 import librosa
 import soundfile as sf
+import pyloudnorm as pyln
+import pandas as pd
+from tqdm import tqdm
 
 
 def get_files(data_dir, file_format=".flac"):
@@ -23,6 +26,18 @@ def get_files(data_dir, file_format=".flac"):
                 files.append(filename)
 
     return list(zip(filepaths, files))
+
+
+def get_speakers(input_dir, file_format=".flac"):
+    """
+    Get a list of all unique speakers.
+    """
+    speakers = []
+    for filepath, filename in get_files(input_dir, file_format):
+        speaker_id = filename[:filename.index("-")]
+        speakers.append(speaker_id)
+
+    return list(np.unique(speakers))
 
 
 def select_audio(audio, sample_rate, s):
@@ -45,12 +60,16 @@ class CustomDataGenerator(tf.keras.utils.Sequence):
     def __init__(self, data_path,
                  data_files, dim, 
                  max_k, batch_size,
+                 mean, std, s,
                  shuffle=True):
         
         self.data_path = data_path
         self.data_files = data_files
         self.dim = dim
         self.batch_size = batch_size
+        self.mean = mean
+        self.std = std
+        self.s = s
         self.shuffle = shuffle
         
         self.n = len(self.data_files)
@@ -71,17 +90,29 @@ class CustomDataGenerator(tf.keras.utils.Sequence):
         return X, y
 
     def __getX(self, file_name):
+        # Read audio
         path = os.path.join(self.data_path, file_name)
         audio_file = tf.io.read_file(path)
         audio, sample_rate = tf.audio.decode_wav(contents=audio_file)
         audio = tf.squeeze(audio, axis=-1)
-        audio = select_audio(audio, sample_rate, 5)
+        
+        # Select random s seconds sample
+        audio = select_audio(audio, sample_rate, self.s)
 
+        # Convert to frequency domain
         waveform = tf.cast(audio, dtype=tf.float32)
         samples = int(sample_rate.numpy() * (25/1000))
         overlap = int(sample_rate.numpy() * (10/1000))
         spectrogram = tf.signal.stft(waveform, frame_length=samples, frame_step=overlap, fft_length=samples, pad_end=True)
-        spectrogram = tf.abs(spectrogram) # not sure
+        spectrogram = tf.abs(spectrogram) 
+
+        # Normalize
+        spectrogram, _ = tf.linalg.normalize(spectrogram, 'euclidean')
+
+        # Standardize
+        spectrogram = (spectrogram - self.mean) / self.std
+
+        # Add batch dimension
         spectrogram = spectrogram[..., tf.newaxis]
 
         return spectrogram
@@ -103,16 +134,79 @@ class CustomDataGenerator(tf.keras.utils.Sequence):
         return self.n // self.batch_size
 
 
-def process_noise(data_dir, dataset, noise_samples, sample_rate):
+def process_noise(data_dir, dataset, noise_samples, sample_rate, duration=10):
     """
     Process noise samples by resampling the audio to the same
     sample rate as the speaker audio and saving them in the
     correct dataset directories.
     """
-    for i, noise_sample in enumerate(noise_samples):
+    for i, noise_sample in tqdm(enumerate(noise_samples)):
         # Load audio with new sample rate
-        audio, samplerate = librosa.load(noise_sample[0], sr=sample_rate)
+        audio, samplerate = librosa.load(noise_sample[0], sr=sample_rate, duration=duration)
+
+        # Peak normalize
+        normalized_audio = pyln.normalize.peak(audio, 0)
 
         # Save new audio 
         new_name = os.path.join(data_dir, f"{dataset}/", f"{0:02}_speakers_{i}.wav")
-        sf.write(new_name, audio, sample_rate)
+        sf.write(new_name, normalized_audio, sample_rate)
+
+
+def remove_speech_noise(files, meta_path, speech_categories):
+    """
+    Remove all noise files that contain speech based
+    on the speech categories mentioned in the paper. 
+    """
+    # Read meta data
+    noise_df = pd.read_csv(meta_path, sep="\t", header=None, names=["filename", "category", "code"])
+
+    # Remove path
+    noise_df["filename"] = noise_df["filename"].str.replace("audio/", "")
+
+    # Create regular expression
+    speech_categories = "|".join(speech_categories)                    
+
+    # Keep rows that contain the speech categories
+    noise_df = noise_df[noise_df["category"].str.contains(speech_categories)]
+
+    # Get speech-containing noise filenames
+    speech_files = noise_df["filename"].tolist()
+
+    # Remove from files
+    for f in files:
+        if f[1] in speech_files:
+            files.remove(f)
+
+    return files
+
+
+def get_train_mean_std(input_dir):
+    """
+    Compute the mean and standard deviation over
+    the training set to be used for standardization
+    over both the training set, as well as
+    validation and test sets. 
+    """
+    spectrograms = []
+
+    for filepath, filename in tqdm(get_files(input_dir, file_format=".wav")):
+        # Read resampled audio
+        audio_file = tf.io.read_file(filepath)
+        audio, sample_rate = tf.audio.decode_wav(contents=audio_file)
+        audio = tf.squeeze(audio, axis=-1)
+        waveform = tf.cast(audio, dtype=tf.float32)
+
+        # Convert to frequency domain
+        samples = int(sample_rate.numpy() * (25/1000))
+        overlap = int(sample_rate.numpy() * (10/1000))
+        spectrogram = tf.signal.stft(waveform, frame_length=samples, frame_step=overlap, fft_length=samples, pad_end=True)
+        spectrogram = tf.abs(spectrogram) 
+
+        # Normalize 
+        spectrogram, _ = tf.linalg.normalize(spectrogram, 'euclidean')
+
+        # Collect all spectrograms
+        spectrograms.append(spectrogram.numpy())
+
+    # Return mean and standard deviation
+    return np.mean(spectrograms), np.std(spectrograms)